a47a7eac8b5166f33ad9e5fb5b9c9ef1b584413c
Enolley <enolaomukamani@gmail.com>
2026-02-11 15:36:45 +0300

    security

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use App\Http\Middleware\AdminOnly;
 use App\Http\Middleware\ReviewerOnly;
 
@@ -12,12 +13,20 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function ($middleware) {
+    ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO
+        );
+
         $middleware->alias([
             'admin' => AdminOnly::class,
             'reviewer' => ReviewerOnly::class,
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
