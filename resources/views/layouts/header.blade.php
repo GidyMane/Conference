02fe5d8470fa94af300c57c6e9d0660ded5ca555
Enolley <enolaomukamani@gmail.com>
2026-02-11 15:08:09 +0300

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -87,7 +87,7 @@
     </header>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark kalro-nav">
+    <nav class="navbar navbar-expand-lg navbar-dark kalro-nav" style="background: linear-gradient(135deg, #198754 0%, #146c43 100%) !important; box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);">
         <div class="container">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
                 <span class="navbar-toggler-icon"></span>
