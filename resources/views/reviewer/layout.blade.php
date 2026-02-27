0c5c11e1fa4bada567f93fd2b69a781eb627bf09
GidyMane <muragegideon2000@gmail.com>
2026-02-27 20:42:06 +0300

    fix the issue with active link

--- resources/views/reviewer/layout.blade.php
+++ resources/views/reviewer/layout.blade.php
@@ -370,14 +370,15 @@
                 <i class="fas fa-check-circle"></i>
                 <span>Completed Reviews</span>
             </a>
-             <a href="{{ url('/reviewer/fullpapers-completed') }}" class="menu-item {{ request()->is('reviewer/fullpapers-completed*') || request()->is('reviewer/fullpapers/*/all-reviews*') ? 'active' : '' }}">
-    <i class="fas fa-clipboard-list"></i>
-    <span>Fully Reviewed Papers</span>
-</a>
             
-            <a href="{{ route('reviewer.fullpapers.index') }}" class="menu-item {{ request()->is('reviewer/fullpapers*') ? 'active' : '' }}">
+            <a href="{{ route('reviewer.fullpapers.index') }}" class="menu-item {{ request()->is('reviewer/fullpapers') || request()->is('reviewer/fullpapers/*') && !request()->is('reviewer/fullpapers/*/all-reviews') ? 'active' : '' }}">
     <i class="fas fa-book-reader"></i>
     <span>Full Papers</span>
+</a>
+
+            <a href="{{ url('/reviewer/fullpapers-completed') }}" class="menu-item {{ request()->is('reviewer/fullpapers-completed') || request()->is('reviewer/fullpapers/*/all-reviews') ? 'active' : '' }}">
+    <i class="fas fa-clipboard-list"></i>
+    <span>Fully Reviewed Papers</span>
 </a>
 
             
